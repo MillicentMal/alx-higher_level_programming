e787d08e0e4c101a0e1019a0796b9b045252b5e6
MillicentMal <millicentmalinga19@gmail.com>
2021-06-02 09:03:19 +0000

    we try again

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):                      
-    unique_list = []                           
-    i = len(my_list) - 1                       
-                                               
-    while i > 1:                               
-        j = 0                                  
-        while j < i:                           
-        # If the value on the left is bigger sw
-            if my_list[j] > my_list[j+1]:      
-                                               
-                temp = my_list[j]              
-                my_list[j] = my_list[j+1]      
-                my_list[j+1] = temp            
-            else:                              
-                 j += 1                        
-        i -= 1                                 
-    print(my_list)                             
-                                               
-    k = len(my_list) - 1                       
-    while k >= 0:                              
-        if my_list[k] != my_list[k-1]:         
-            unique_list.append(my_list[k])     
-        k -= 1                                 
-                                               
-    m = 0                                      
-    result = 0                                 
-    while m != len(unique_list):               
-        result = result + unique_list[m]       
-        m += 1                                 
-    return result                              
+
+# only sums up the unique characters of a list
+
+def uniq_add(my_list=[]):
+
+   uniq_list = list(set(my_list))
+
+   result = 0
+
+
+   for i in range(0, len(uniq_list)):
+       result += uniq_list[i]
+
+
+   return result
+                              
