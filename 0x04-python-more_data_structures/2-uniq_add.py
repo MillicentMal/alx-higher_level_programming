c37e042a67ecb4f1ace2ad710dbf6acb569190c9
MillicentMal <millicentmalinga19@gmail.com>
2021-06-02 09:10:36 +0000

    pep issues

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,15 +3,8 @@
 # only sums up the unique characters of a list
 
 def uniq_add(my_list=[]):
-
-   uniq_list = list(set(my_list))
-
-   result = 0
-
-
-   for i in range(0, len(uniq_list)):
-       result += uniq_list[i]
-
-
-   return result
-                              
+    uniq_list = list(set(my_list))
+    result = 0
+    for i in range(0, len(uniq_list)):
+        result += uniq_list[i]
+    return result                              
