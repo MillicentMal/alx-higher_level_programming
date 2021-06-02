5c7190b2b7650c0032c724c252ef76c32a56f197
MillicentMal <millicentmalinga19@gmail.com>
2021-06-02 12:44:28 +0000

    Technical interview my foot. I would have failed this one

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# Python code to convert string to list
+def roman_to_int(roman_string):
+    roman_number = []
+    if roman_string == None and type(roman_string) != str:
+        return 0
+
+    for roman in roman_string:
+        roman_number.append(roman)
+
+    roman_to_int = 0
+    for roman in roman_number:
+        if roman == 'I':
+            roman_to_int += 1
+        elif roman == 'V':
+            roman_to_int += 5
+        elif roman == 'X':
+            roman_to_int += 10
+        elif roman == 'L':
+            roman_to_int += 50
+        elif roman == 'C':
+            roman_to_int += 100
+        elif roman == 'D':
+            roman_to_int += 500
+        elif roman == 'M':
+            roman_to_int += 1000
+        else:
+            print("Could not continue. Non-roman numeral letter found")
+            break
+    return roman_to_int
