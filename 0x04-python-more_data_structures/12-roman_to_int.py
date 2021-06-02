060cbb6e99674563cfe13eb690babf2c86e7a13d
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-06-02 20:51:34 +0200

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 # Python code to convert string to list
 def roman_to_int(roman_string):
     roman_number = []
@@ -10,27 +10,21 @@ def roman_to_int(roman_string):
 
     roman_to_int = 0
     for roman in roman_number:
-        i = len(roman_string)
-        while i => 0:
-            i -= 1
-            if roman_string[i -1] == 'I' & roman_string[i] != 'I':
-                roman_to_int -= 1
-            else:
-                
-                if roman == 'I':
-                    roman_to_int += 1
-                elif roman == 'V':
-                    roman_to_int += 5
-                elif roman == 'X':
-                    roman_to_int += 10
-                elif roman == 'L':
-                    roman_to_int += 50
-                elif roman == 'C':
-                    roman_to_int += 100
-                elif roman == 'D':
-                    roman_to_int += 500
-                elif roman == 'M':
-                    roman_to_int += 1000
-                
-     
+        if roman == 'I':
+            roman_to_int += 1
+        elif roman == 'V':
+            roman_to_int += 5
+        elif roman == 'X':
+            roman_to_int += 10
+        elif roman == 'L':
+            roman_to_int += 50
+        elif roman == 'C':
+            roman_to_int += 100
+        elif roman == 'D':
+            roman_to_int += 500
+        elif roman == 'M':
+            roman_to_int += 1000
+        else:
+            print("Could not continue. Non-roman numeral letter found")
+            break
     return roman_to_int
