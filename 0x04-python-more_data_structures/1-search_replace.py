2fbcd0aadf76e2f1f719ead722007c1ae86ad9fd
MillicentMal <millicentmalinga19@gmail.com>
2021-06-02 00:03:43 +0000

    New update

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    i = 0
+    while i != len(my_List):
+        if my_List[i] != search:
+            new_List.append(my_List[i])
+        else:
+            new_List.append(replace)
+    return new_List
+
