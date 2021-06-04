a2be858814e7b516a669a3211437cc9d06397331
MillicentMal <millicentmalinga19@gmail.com>
2021-06-04 22:33:56 +0000

    Lets get that 50%

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        my_list = list(a_dictionary.keys())
-        score = 0
-        highest = ""
-        for i in my_list:
-            if a_dictionary[i] > score:
-                score = a_dictionary[i]
-                highest = i
-    return highest
+        return max(a_dictionary, key=a_dictionary.get)
+    else:
+        return None
