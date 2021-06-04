a7b06de015bc4c5c0cebd526eeb3f217b5a994c9
MillicentMal <millicentmalinga19@gmail.com>
2021-06-04 22:24:13 +0000

    At some point I typed "git vommit" Probably my brain telling me the kind of stuff I am about to push to this github

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if key in a_dictionary:
-        return max(a_dictionary, key=a_dictionary.get)
-    else:
-        return None
+    if a_dictionary:
+        my_list = list(a_dictionary.keys())
+        score = 0
+        highest = ""
+        for i in my_list:
+            if a_dictionary[i] > score:
+                score = a_dictionary[i]
+                highest = i
+    return highest
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,30 +1,15 @@
-!/usr/bin/python3
-# Python code to convert string to list
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_number = []
-    if roman_string == None and type(roman_string) != str:
-        return 0
-
-    for roman in roman_string:
-        roman_number.append(roman)
-
-    roman_to_int = 0
-    for roman in roman_number:
-        if roman == 'I':
-            roman_to_int += 1
-        elif roman == 'V':
-            roman_to_int += 5
-        elif roman == 'X':
-            roman_to_int += 10
-        elif roman == 'L':
-            roman_to_int += 50
-        elif roman == 'C':
-            roman_to_int += 100
-        elif roman == 'D':
-            roman_to_int += 500
-        elif roman == 'M':
-            roman_to_int += 1000
-        else:
-            print("Could not continue. Non-roman numeral letter found")
-            break
-    return roman_to_int
+    num_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if num_map[roman_string[i]] >= num_map[roman_string[i + 1]]:
+                total += num_map[roman_string[i]]
+            else:
+                total -= num_map[roman_string[i]]
+            i += 1
+        total += num_map[roman_string[i]]
+        return total
+    return 0
