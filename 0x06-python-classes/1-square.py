121866f5449fb2a44b82714375d73fb4bc7f9ec5
MillicentMal <millicentmalinga19@gmail.com>
2021-06-20 15:43:44 +0000

    I AM TRYING MY BEST

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This is a square class that defines a square
+"""
+
+
+class Square:
+    def __init__(self, size):
+        """Defines a private attribute size
+        """
+
+        self.__size = size
+        size = Square()
