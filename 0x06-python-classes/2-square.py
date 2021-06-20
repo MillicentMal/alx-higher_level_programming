eceec16472ac7d894762c165858771ef6d21e8b2
MillicentMal <millicentmalinga19@gmail.com>
2021-06-20 16:30:21 +0000

    ...

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,19 +8,10 @@ class Square:
     """
     def __init__(self, size=0):
         """Optional instantiation
-    """
-        self.size = size 
-    @property
-    def size(self):
-        """Getter method
-    """
-        print("Retrieving square size")
-        return self.__size
+  """
+       
 
-    @size.setter
-    def size(self, value):
-        """Setter method
-     """
+        self.__size = size
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
