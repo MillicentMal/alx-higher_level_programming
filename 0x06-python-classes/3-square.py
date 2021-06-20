ef1480930fd4689d68e7ece3ee8155d7d85389df
MillicentMal <millicentmalinga19@gmail.com>
2021-06-20 16:54:49 +0000

    I hate this

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is a square class that defines a square
+"""
+
+
+class Square:
+    """This is a square class that defines a square
+    """
+    def __init__(self, size=0):
+        """Optional instantiation
+  """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """gives area of Square
+    """
+        return self.__size ** self.__size
