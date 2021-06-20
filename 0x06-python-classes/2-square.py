167a2764c44173c9c5a4bfa32ef077551f715dd6
MillicentMal <millicentmalinga19@gmail.com>
2021-06-20 16:33:02 +0000

    I would like to sleep

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,9 +12,9 @@ class Square:
        
 
         self.__size = size
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        if value < 0:
+        if not isinstance(size, int):
+             raise TypeError("size must be an integer")
+        elif value < 0:
             raise ValueError("size must be >= 0")
         print("Setting size")
         self.__size = size
