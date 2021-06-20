3614ea910cdee0c4f7e64f20577e06bf2a7f9811
MillicentMal <millicentmalinga19@gmail.com>
2021-06-20 16:16:43 +0000

    Lol

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This is a square class that defines a square
+"""
+
+
+class Square:
+    """This is a square class that defines a square
+    """
+    def __init__(self, size):
+        """Defines a private attribute size
+    """
+
+        self.__size = size
+    def __init__(self, size=0):
+        """Optional instantiation
+    """
+        self.size = size 
+    @property
+    def size(self):
+        """Getter method
+    """
+        print("Retrieving square size")
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method
+     """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        print("Setting size")
+        self.__size = size
+        size = Square(value)
