3384f6918a81676a71f023c165f1cfaaced1c426
MillicentMal <millicentmalinga19@gmail.com>
2021-06-28 09:34:09 +0000

    Fix pep8 error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""This is a square class that defines a square
+"""
+class Square that defines a square by:
+(based on 0-square.py)
 """
 
 
 class Square:
-    """This is a square class that defines a square
+    """Square class with a private attribute -
+    size.
     """
+
     def __init__(self, size):
-        """Defines a private attribute size
-    """
-
-        self.__size = size
-
-    @property
-    def size(self):
-        """Getter method
-    """
-        print("Retrieving square size")
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Setter method
+        """Initializes the size variable as a private
+        instance artribute
         """
-        print("Setting size")
         self.__size = size
-        size = Square(value)
