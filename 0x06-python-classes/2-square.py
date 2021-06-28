1faf53f5a9e4aa03b0bd800445a80219808858c2
MillicentMal <millicentmalinga19@gmail.com>
2021-06-28 09:41:13 +0000

    Fix pep8 Error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,10 +9,8 @@ class Square:
     def __init__(self, size=0):
         """Optional instantiation
   """
-       
-
         self.__size = size
         if not isinstance(size, int):
-             raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
