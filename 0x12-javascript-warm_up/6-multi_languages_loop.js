7fbea87e77175eca730271bdfa0d6820fa72c450
MillicentMal <millicentmalinga19@gmail.com>
2021-08-02 12:46:20 +0000

    Task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-/*
-script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-*/
 
 const Languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < Languages.length; i++) {
