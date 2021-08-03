9b239308ccea473e210aeebbe4ee8aadd3fdbecc
MillicentMal <millicentmalinga19@gmail.com>
2021-08-03 21:43:43 +0000

    Task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+exports.logMe = function (item) {
+// Output format: <number arguments already printed>: <current argument value>
+
+  let i = 0;
+  console.log(i + ':' + item);
+  i++;
+};
