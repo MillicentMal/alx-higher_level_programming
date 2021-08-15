9c7fe8440881c7d2cae86ab62e1e6c1878fa2403
MillicentMal <millicentmalinga19@gmail.com>
2021-08-15 16:19:26 +0000

    Task 5

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- creates a table, first_table with values id and name.
 CREATE TABLE IF NOT EXISTS `first_table` 
-(`id` INT, `name` VARCHAR(256));ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci
+(`id` INT NOT NULL AUTO INCREMENT, `name` VARCHAR(256));ENGINE=InnoDBDEFAULTCHARSET=utf8mb4COLLATE=utf8mb4_0900_ai_ci
