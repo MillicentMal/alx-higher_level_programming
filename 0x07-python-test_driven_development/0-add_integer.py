bc6ead494df0d78ed2a9e1c6b4b28fd9772ed61b
MillicentMal <millicentmalinga19@gmail.com>
2021-08-25 09:53:13 +0200

    Test driven development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Adds a and b if only a and b are integers or floats. 
+return sum of a and b """
+
+def add_integer(a, b=98):
+    return a + b
+    
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+The directory contains test driven development practices
