437189c0e4e50d2cd2b9eb2a920a49ea1518aee4
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-09-11 23:19:09 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,18 +5,22 @@
 
 import MySQLdb
 from sys import argv
+from sqlalchemy import create_engine
+
 
 if __name__ == '__main__':
+    
     user, password, database = argv[1], argv[2], argv[3]
     db = MySQLdb.connect(
   host="localhost",
+        port="3306",
   user="user",
   password="password",
 db="database")
 
-x = "SELECT * FROM states ORDER by id"
+x = db.query(states).order_by(states.id)
 
-mycursor=my_conn.execute(x)
-my_data=mycursor.fetchall()
+connection = my_conn.execute(x)
+my_data=connection.fetchall()
 for row in my_data:
     print(row)
