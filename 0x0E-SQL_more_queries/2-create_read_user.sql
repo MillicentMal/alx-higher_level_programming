8257c2ff7c4da783aeddbe22047b2bb52b3f1e8b
MillicentMal <millicentmalinga19@gmail.com>
2021-10-01 18:23:14 +0000

    Task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates user user_0d_1 
-CREATE IF NOT EXISTS USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-CREATE IF NOT EXISTS DATABASE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT PRIVILEGES ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
 
