44f57b40e01943567ae58401fd7cc117dad561ea
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2021-10-02 17:00:24 +0200

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
--- creates user user_0d_1 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- creates database and grants select privileges to user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT PRIVILEGES ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2  * TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
 
