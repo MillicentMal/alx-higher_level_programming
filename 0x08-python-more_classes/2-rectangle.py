4b24fc9737ac25430f2f55f6e3f4b0a2b9e4b442
MillicentMal <millicentmalinga19@gmail.com>
2021-10-06 13:52:05 +0000

    Task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a class of a rectangle"""
+
+
+class Rectangle:
+    """Rectangle class"""
+    
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self): 
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Gets the width"""
+        self.__width = value
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        
+
+    @property
+    def height(self):
+         return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Gets the height"""
+        self.__height = value
+
+        if not isinstance(value, int):
+            raise TypeError("value must be an integer")
+        elif value < 0:
+            raise ValueError("valu must be >= 0")
+       
