45352b350049d087a8625eae8859a42a8f9da190
MillicentMalinga <m.malinga@alustudent.com>
2021-10-30 18:29:59 +0200

    Removes test code

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,12 +8,3 @@ def say_my_name(first_name, last_name=""):
         print("My name is " + first_name + " " + last_name)
     else:
         raise TypeError("first_name must be a string or last_name must be a string")
-
-
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -8,27 +8,12 @@ def print_square(size):
 
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
-    if isinstance(size, int)  and size < 0:
+    if isinstance(size, int) and size < 0:
         raise ValueError("size must be >= 0")
     elif isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     else:
         for i in range(size):
             for j in range(size):
-                 print("#",  end="")
+                print("#", end="")
             print()
-
-
-print_square(4)
-print("")
-print_square(10)
-print("")
-print_square(0)
-print("")
-print_square(1)
-print("")
-try:
-    print_square(-1)
-except Exception as e:
-    print(e)
-print("")
