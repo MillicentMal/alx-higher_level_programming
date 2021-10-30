18cfb2af46150a8949899df23faff90cdb23d00f
MillicentMalinga <m.malinga@alustudent.com>
2021-10-30 18:21:21 +0200

    Added new files

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,6 +2,24 @@
 """Adds a and b if only a and b are integers or floats. 
 return sum of a and b """
 
+
 def add_integer(a, b=98):
-    return a + b
-    
+    """Adds integers a and b and returns their sum"""
+    if not isinstance(b, float)  and  not isinstance(b, int) or not isinstance(a, float)  and not isinstance(a, int) :
+        raise TypeError("a must be an integer or b must be an integer")
+    else:
+        return int(a) + int(b)
+
+
+print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
