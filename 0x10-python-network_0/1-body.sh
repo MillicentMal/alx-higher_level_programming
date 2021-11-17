d8aad8838190da1743c1057c7ffddb4448dd389c
MillicentMal <millicentmalinga19@gmail.com>
2021-11-17 13:21:38 +0000

    Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -5,4 +5,12 @@ curl -i "$1" | grep -i Content-Length | awk '{print $2}'
 =======
 # displays body of response to GET request
 curl -sL "$1" -X GET
+<<<<<<< HEAD
 >>>>>>> 24649d0 (Task 1)
+=======
+<<<<<<< HEAD
+>>>>>>> cb11bcb3feee63f84e504712a5a26bb09f9b0558
+=======
+>>>>>>> 24649d0 (Task 1)
+>>>>>>> c604332 (Task 1)
+>>>>>>> 9c6fe79 (Task 1)
