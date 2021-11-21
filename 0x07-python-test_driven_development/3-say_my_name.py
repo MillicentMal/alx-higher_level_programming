17ec7c7c63fd986c5cd72e814042daa10f5f26c0
MillicentMal <millicentmalinga19@gmail.com>
2021-11-21 10:49:24 +0000

    Task 3 tests

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,7 +4,10 @@ def say_my_name(first_name, last_name=""):
     returns the string 'My name is first_name + last_name'
     """
 
-    if isinstance(first_name, str) and isinstance(last_name, str):
-        print("My name is " + first_name + " " + last_name)
+    if (not isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+    if (not isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
     else:
-        raise TypeError("first_name must be a string or last_name must be a string")
+        print("My name is " + first_name + " " + last_name)
+
