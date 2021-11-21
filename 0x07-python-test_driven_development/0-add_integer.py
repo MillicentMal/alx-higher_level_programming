477216f2bd937eb503a1c55ad33e07aa0e557cfe
MillicentMal <millicentmalinga19@gmail.com>
2021-11-21 09:43:25 +0000

    Tests for Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,8 +4,15 @@ return sum of a and b """
 
 
 def add_integer(a, b=98):
-    """Adds integers a and b and returns their sum"""
-    if not isinstance(b, float) and not isinstance(b, int) or not isinstance(a, float) and not isinstance(a, int):
-        raise TypeError("a must be an integer or b must be an integer")
+    """Add a to b and return the result.
+    All arguments that are floats will be typecasted to ints
+    Raises:
+        TypeError: when a or b are neither integers nor floats.
+    """
+    
+    if ((not isinstance(b, float) and not isinstance(b, int))):
+        raise TypeError("b must be an integer")
+    if ((not isinstance(a, float) and not isinstance(a, int))):
+        raise TypeError("a must be an integer")
     else:
         return int(a) + int(b)
