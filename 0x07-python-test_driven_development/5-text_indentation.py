0a5cb22abbbe7edf826fd99ce637db6abdc07ae0
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2022-02-04 20:43:59 +0200

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,12 +11,12 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     else:
         for i in range(len(text)):
-        if text[i] in delimiters:
-            end = i
-            print(text[start:end+1])
-            print()
-            print()
-            start = i + 1
-    print(text[end + 1:], end="")
+            if text[i] in delimiters:
+                end = i
+                print(text[start:end+1])
+                print()
+                print()
+                start = i + 1
+        print(text[end + 1:], end="")
                 
     
