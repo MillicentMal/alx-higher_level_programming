e5f081aeeb93f11d09fe2e07c4ae5c2b7e967843
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2022-02-05 00:33:50 +0200

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-
+"""
+text Indentation
+"""
 
 def text_indentation(text):
     """
     prints a text with 2 new lines after each of these characters: ., ? and :
     """
     start = 0
-    end = 0
     delimiters = ['.', '?', ':']
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     else:
-    delimiters = ['.', '?', ':']
-    new_string = text
-    start = 0
-    for i in range(len(text)):
-        if text[i] in delimiters:
-            print(text[start:i+1].lstrip('.:?').strip())
-            print()
-            print()
-            start = i
-            new_string = text[i:]
-    print(new_string.lstrip(':.?'))
+        new_string = text
+        start = 0
+        for i in range(len(text)):
+            if text[i] in delimiters:
+                print(text[start:i+1].lstrip('.:?').strip())
+                print()
+                print()
+                start = i
+                new_string = text[i:]
+        print(new_string.lstrip(':.?'))
                 
     
