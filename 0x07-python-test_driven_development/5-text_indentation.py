48c9fb25a22059186bb949fe5fd53792ed6eb7f5
MillicentMal <millicentmalinga19@gmail.com>
2022-02-04 08:03:02 -0800

    Task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,15 +5,15 @@ def text_indentation(text):
     """
     prints a text with 2 new lines after each of these characters: ., ? and :
     """
-    new_string = []
+    new_string = ''
     delimiters = ['.', '?', ':']
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     else:
         for i in text:
-            new_string.append(i)
+            new_string += i
             if i in delimiters:
-                print(''.join(new_string))
-                print()
+                print(new_string)
                 print()
                 new_string.clear()
+            print(new_string)
