7fd4138e3425139cecca4691ab7cdb48b64086aa
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-07 10:50:26 +0200

    Matrix assignment

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
-    try:
-        return map(lambda x, y: round(x / y , 2), matrix, div)
-    except:
-        if div == 0:
-            return ZeroDivisionError("division by zero")
-        elif not isinstance(div, int) or not isinstance(div, float):
-            return TypeError("div must be a number")
-        else:
-            for i in range(len(matrix)):
-                if not isinstance(matrix[i], list):
-                    return TypeError("matrix must be a matrix (list of lists) of integers/floats")
-                elif len(matrix[i]) != len(matrix[i + 1]):
-                    return TypeError("Each row of the matrix must have the same size")
-                else:
-                    for num in matrix[i]:
-                        if not isinstance(num, int) or not isinstance(num, float):
-                            return TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    
-
+    list_length = []
+    for i in range(len(matrix)):
+        list_length.append(len(matrix[i]))
+        for num in matrix[i]:
+            if not isinstance(num, int) and not isinstance(num, float):
+                return TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            elif not isinstance(matrix[i], list):
+                return TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if len(set(list_length)) > 1:
+        return TypeError("Each row of the matrix must have the same size")
+    elif div == 0:
+        return ZeroDivisionError("division by zero")
+    elif not isinstance(div, int) and not isinstance(div, float):
+        return TypeError("div must be a number")       
+            
+    new_matrix = []
+    for i in matrix:
+        new_matrix.append(list(map(lambda x: round(x / div, 2), i)))
+    return new_matrix   
