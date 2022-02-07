1f2b29a9fb389382b53276ea4b0cccfe2d8e0db6
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-07 12:32:47 +0200

    pep8 errors on matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-    Matrix division 
+    Matrix division
     """
+
 
 def matrix_divided(matrix, div):
     """[summary]
 
     Args:
-        matrix (List): of integers and floats 
+        matrix (List): of integers and floats
         div (int / float): the number to divide matrix with
 
     Returns:
@@ -16,21 +17,26 @@ def matrix_divided(matrix, div):
     list_length = []
     for i in range(len(matrix)):
         if not isinstance(matrix[i], list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         list_length.append(len(matrix[i]))
         for num in matrix[i]:
             if not isinstance(num, int) and not isinstance(num, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) \
+                    of integers/floats")
             elif not isinstance(matrix[i], list):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) \
+                    of integers/floats")
     if len(set(list_length)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
     elif div == 0:
         raise ZeroDivisionError("division by zero")
     elif not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("div must be a number")       
-            
+        raise TypeError("div must be a number")
+
     new_matrix = []
     for i in matrix:
         new_matrix.append(list(map(lambda x: round(x / div, 2), i)))
-    return new_matrix   
+    return new_matrix
