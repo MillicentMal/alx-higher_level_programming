d1e7b382a69fee6dc0213336c8b8bb165a36f626
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-08 09:33:30 +0200

    fix errors

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,13 @@
+#!/usr/bin/python3
+""" 5-base_geometry: class BaseGeometry """
+
+
 class BaseGeometry:
+    """
+        class BaseGeometry
+    """
     def __init__(self) -> None:
         pass
-
 
 bg = BaseGeometry()
 
