c4f980ee2a2c87094d95975c6bf808e8542045d6
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-08 09:14:37 +0200

    PEP8 Errors

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" 
- returns True if the object is an instance of a class that 
- inherited (directly or indirectly) from the specified class ; otherwise False.
+"""
+returns True if the object is an instance of a class that
+inherited (directly or indirectly) from the specified class ; otherwise False.
 """
 
 
 def inherits_from(obj, a_class):
-    """ 
-     returns True if the object is an instance of a class 
-     that inherited (directly or indirectly) 
-     from the specified class ; otherwise False.
+    """
+    returns True if the object is an instance of a class
+    that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
