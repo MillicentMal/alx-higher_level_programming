0007b837757fe3aacd007862d6c295e446bf2ece
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-08 08:16:38 +0200

    documenting lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+    Returns list of object attributes and methods
+    """
 def lookup(obj):
     """returns a list of all attributes and methods of a class
     """
     return dir(obj)
+
+print(lookup(int))
