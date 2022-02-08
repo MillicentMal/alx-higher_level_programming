597428a4b686220529cf5ce1e1dd32abce89bee5
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-08 17:37:02 +0200

    Fixing encoding

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,6 +8,5 @@ def read_file(filename=""):
     function that reads a text file (UTF8) and prints it to stdout
     """
     
-    with open(filename, 'r') as textfile:
-        print(textfile.read())
-
+    with open(filename, encoding='UTF-8') as textfile:
+        print(textfile.read(), end='')
