8a7ee04a78572adf88b613777c1abb7c0ea79b20
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-08 08:39:01 +0200

    Fixing return error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -8,7 +8,4 @@ def is_same_class(obj, a_class):
     Returns:
         True or False
     """
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False 
+    return type(obj) is a_class
