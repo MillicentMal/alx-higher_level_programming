7adb9052f24cbe295b737079a2c632e59fbe6929
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-08 08:32:21 +0200

    First push of task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    checks if object is an instance of a specified class
+    """
+    
+def is_same_class(obj, a_class):
+    """
+    Returns:
+        True or False
+    """
+    return isinstance(obj, a_class)
