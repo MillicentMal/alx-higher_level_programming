7c28ab579ae31de6c0b655773873e35737076c5f
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-09 07:54:01 +0200

    test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,11 +6,24 @@ max_integer = __import__('6-max_integer').max_integer
 from parameterized import parameterized
 
 class TestMaxInteger(unittest.TestCase):
-    @parameterized.expand([
-    ([45, 1, 3, 12], 45),
-    ([1, 1, 1, 1], 1),
-    ([], None)
-   ])
-    def test_max_integer(self, integers, expected):
-        self.assertEqual(max_integer(integers), expected)
+    def test_max_integer(self):
+        self.assertEqual(max_integer([45, 1, 3, 12]), 45)
 
+    def test_max_integer_1(self):
+        self.assertEqual(max_integer([45, 1, 200, 3, 12]), 200)
+    
+    def test_max_integer_2(self):
+        self.assertEqual(max_integer([45, 1, 3, 12, 200]), 200)
+    
+    def test_max_integer_3(self):
+        self.assertEqual(max_integer([]), None)
+    
+    def test_max_integer_4(self):
+        self.assertEqual(max_integer([45, 1, 3, -12]), 45)
+    
+    def test_max_integer_5(self):
+        self.assertEqual(max_integer([-45, -1, -3, -12]), -1)
+    
+    def test_max_integer(self):
+        self.assertEqual(max_integer([45]), 45)
+    
