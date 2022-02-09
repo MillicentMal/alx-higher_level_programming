d2f7f3fd8a3d993e495f6627cc6412e8843d4809
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-09 13:19:36 +0200

    first attempt

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ 
+    writing to json file
+    """
+
+    with open(filename, 'w') as json_file:
+        return json_file.write(json.dumps(my_obj))
