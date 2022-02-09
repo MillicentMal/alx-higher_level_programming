f6c2c7f44bf1debf8a5a93a52d27a6b8e68eb1a2
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-09 15:22:47 +0200

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class Base:
+    """ 
+    base class
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """ 
+        initialising objects 
+        """
+        if self.id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
+
