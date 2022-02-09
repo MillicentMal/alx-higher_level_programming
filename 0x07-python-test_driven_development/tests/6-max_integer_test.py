c1337d84a543b177d68194695504dd9e425d56d2
MillicentMal <90625689+MillicentMalinga@users.noreply.github.com>
2022-02-09 07:55:23 +0200

    fixing import error

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -3,7 +3,7 @@
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
-from parameterized import parameterized
+
 
 class TestMaxInteger(unittest.TestCase):
     def test_max_integer(self):
