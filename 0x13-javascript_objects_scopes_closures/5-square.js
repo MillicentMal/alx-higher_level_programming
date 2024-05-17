b0f294b2606642968addb7fca9ae27f5b785b6df
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2024-05-17 13:00:51 +0200

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// square inherits from the Rectangle class found in the 4-Rectangle.js file
-const Rectangle = require('./4-rectangle');
-
-module.exports = class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-};
--- 0x13-javascript_objects_scopes_closures/README.md
+++
@@ -1,2 +0,0 @@
-Focusing on the Objects, Classes and Closures in Javascript.
-If you ask me, this escalated fast
